0cd8a0716b8ecdc8d2bb34ca713ebece59cfffeb
RTLogik <contact@rtlogik.com>
2021-03-31 12:42:06 +0200

    fix style

--- source/hw/rtl/Note_Mixer.vhd
+++ source/hw/rtl/Note_Mixer.vhd
@@ -10,14 +10,14 @@ use IEEE.NUMERIC_STD.ALL;
 entity Note_Mixer is
     port (
         i_Clk        : in  Std_Logic;                              -- Clock signal
-        i_NoteIn_0   : in  Std_Logic_Vector(15 downto 0);          -- Note input 0
-        i_NoteIn_1   : in  Std_Logic_Vector(15 downto 0);          -- Note input 1
-        i_NoteIn_2   : in  Std_Logic_Vector(15 downto 0);          -- Note input 2
-        i_NoteIn_3   : in  Std_Logic_Vector(15 downto 0);          -- Note input 3
-        i_NoteIn_4   : in  Std_Logic_Vector(15 downto 0);          -- Note input 4
-        i_NoteIn_5   : in  Std_Logic_Vector(15 downto 0);          -- Note input 5
-        i_NoteIn_6   : in  Std_Logic_Vector(15 downto 0);          -- Note input 6
-        i_NoteIn_7   : in  Std_Logic_Vector(15 downto 0);          -- Note input 7
+        i_NoteIn_0   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 0
+        i_NoteIn_1   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 1
+        i_NoteIn_2   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 2
+        i_NoteIn_3   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 3
+        i_NoteIn_4   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 4
+        i_NoteIn_5   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 5
+        i_NoteIn_6   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 6
+        i_NoteIn_7   : in  Std_Logic_Vector(15 downto 0);          -- Note input channel 7
         --
         o_NoteMix    : out Std_Logic_Vector(15 downto 0)           -- Ouput note (mix of inputs)                                                         
     );
@@ -57,7 +57,7 @@ begin --======================== ARCHITECTURE ================================--
         if rising_edge(i_Clk) then
             r_NoteSum   <= w_NoteSum;
         end if;
-    end process;
+    end process REGS;
 
     -- OUTPUT --
     o_NoteMix <= Std_Logic_Vector(r_NoteSum(15+EXTRA_BITS downto EXTRA_BITS));
