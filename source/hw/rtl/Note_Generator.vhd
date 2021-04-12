e3314760b612da74558d59fa0f272c95c0938071
RTLogik <contact@rtlogik.com>
2021-04-12 20:40:41 +0200

    Update Note_Generator.vhd

--- source/hw/rtl/Note_Generator.vhd
+++ source/hw/rtl/Note_Generator.vhd
@@ -22,6 +22,8 @@ end Note_Generator;
 
 architecture STRUCTURAL of Note_Generator is
 
+    constant c_ZERO_AMP : Std_Logic_Vector(7 downto 0) := (others => '0');
+
     component DDS is
         generic (g_ACC_WIDTH : Natural := 24);                            -- Accumulator bit width
     
@@ -38,13 +40,12 @@ architecture STRUCTURAL of Note_Generator is
 
     -- Wire signals:
     signal w_FFTW2, w_FFTW4 : Std_Logic_Vector(g_FFTW_WIDTH-1 downto 0);
-    signal w_Wave1, w_Wave2, w_Wave3 : Std_Logic_Vector(15 downto 0);
-    signal w_Note : Signed(15 downto 0);
-    signal w_Amp1, w_Amp2, w_Amp3 : Std_Logic_Vector(7 downto 0);
+    signal w_WaveOut1, w_WaveOut2, w_WaveOut3 : Std_Logic_Vector(15 downto 0);
+    signal w_Wave1, w_Wave2, w_Wave3 : Signed(17 downto 0);
+    signal w_NoteSum : Signed(17 downto 0);
 
     -- Register signals:
-    signal r_Note : Signed(15 downto 0) := (others => '0');
-    signal r_Amp1, r_Amp2, r_Amp3 : Std_Logic_Vector(7 downto 0) := (others => '0');
+    signal r_NoteSum : Signed(17 downto 0) := (others => '0');
 
 begin --===================== ARCHITECTURE ===============================-
 
@@ -61,10 +62,10 @@ begin --===================== ARCHITECTURE ===============================-
             i_Clk        => i_Clk,
             i_Enable     => i_Enable,
             i_FTW        => i_FFTW,
-            i_Amp        => r_Amp1,
+            i_Amp        => i_Amp123(7 downto 0),
             i_WaveSelect => i_WaveSel123(1 downto 0),
             --
-            o_Wave       => w_Wave1
+            o_Wave       => w_WaveOut1
         );
 
     DDS_2 : DDS  -- 2nd HARMONIC
@@ -74,10 +75,10 @@ begin --===================== ARCHITECTURE ===============================-
             i_Clk        => i_Clk,
             i_Enable     => i_Enable,
             i_FTW        => w_FFTW2,
-            i_Amp        => r_Amp2,
+            i_Amp        => i_Amp123(15 downto 8),
             i_WaveSelect => i_WaveSel123(3 downto 2),
             --
-            o_Wave       => w_Wave2
+            o_Wave       => w_WaveOut2
         );
 
     DDS_3 : DDS  -- 4th HARMONIC
@@ -87,43 +88,45 @@ begin --===================== ARCHITECTURE ===============================-
             i_Clk        => i_Clk,
             i_Enable     => i_Enable,
             i_FTW        => w_FFTW4,
-            i_Amp        => r_Amp3,
+            i_Amp        => i_Amp123(23 downto 16),
             i_WaveSelect => i_WaveSel123(5 downto 4),
             --
-            o_Wave       => w_Wave3
+            o_Wave       => w_WaveOut3
         );
 
-    --- Amplitude control to avoid overflow in note addition
-	AMP_CTRL: process(i_Amp123)
-	begin
-	       w_Amp1 <= "0" & i_Amp123(7 downto 1);
-	       w_Amp2 <= "0" & i_Amp123(15 downto 9);
-	       w_Amp3 <= "0" & i_Amp123(23 downto 17);
-        if (i_Amp123(15 downto 8) /= "00000000" AND i_Amp123(23 downto 16) /= "00000000") then
-	       w_Amp1 <= "00" & i_Amp123(7 downto 2);
-	       w_Amp2 <= "00" & i_Amp123(15 downto 10);
-	       w_Amp3 <= "00" & i_Amp123(23 downto 18);
-	   end if;	       
-	end process;
-	    
+    
     -----------------------------------------------------
     -- NOTE MIXER
     -----------------------------------------------------
-    w_Note <= Signed(w_Wave1) + Signed(w_Wave2) + Signed(w_Wave3); 
+    w_Wave1 <= resize(Signed(w_WaveOut1), 18); 
+    w_Wave2 <= resize(Signed(w_WaveOut2), 18);
+    w_Wave3 <= resize(Signed(w_WaveOut3), 18);
+    
+    w_NoteSum <= w_Wave1 + w_Wave2 + w_Wave3; 
 
 
     --- REGISTERS ---
     REGS: process (i_Clk)
     begin
         if rising_edge(i_Clk) then
-            r_Note <= w_Note;
-            r_Amp1 <= w_Amp1;
-            r_Amp2 <= w_Amp2;
-            r_Amp3 <= w_Amp3;
+            r_NoteSum <= w_NoteSum;
         end if;
     end process REGS;
 
     --- OUTPUT ---
-    o_Note <= Std_Logic_Vector(r_Note);
+    o_Note <= Std_Logic_Vector(r_NoteSum(15 downto 0)) when 
+              (i_Amp123(23 downto 16) = c_ZERO_AMP AND i_Amp123(15 downto 8) = c_ZERO_AMP) else
+              
+              Std_Logic_Vector(r_NoteSum(16 downto 1)) when 
+              (i_Amp123(23 downto 16) = c_ZERO_AMP AND i_Amp123(15 downto 8) /= c_ZERO_AMP) else
+              
+              Std_Logic_Vector(r_NoteSum(16 downto 1)) when 
+              (i_Amp123(23 downto 16) /= c_ZERO_AMP AND i_Amp123(15 downto 8) = c_ZERO_AMP) else
+              
+              Std_Logic_Vector(r_NoteSum(17 downto 2)) when 
+              (i_Amp123(23 downto 16) /= c_ZERO_AMP AND i_Amp123(15 downto 8) /= c_ZERO_AMP) else
+              
+              (others => '0');
+          
 
 end architecture;
